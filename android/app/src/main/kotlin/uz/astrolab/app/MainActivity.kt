547e64e0ca8bc7f95a435e92910fe46310d5744b
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-08-31 17:00:56 +0500

    completed app

--- android/app/src/main/kotlin/uz/astrolab/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package uz.astrolab.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,7 @@
-const baseUrl = "https://e-work.up.railway.app";
+import 'package:flutter_defualt_project/utils/images.dart';
+import 'package:flutter_defualt_project/utils/musics.dart';
 
+const baseUrl = "https://e-work.up.railway.app";
 
 class TimeOutConstants {
   static int connectTimeout = 30;
@@ -8,3 +10,62 @@ class TimeOutConstants {
 }
 
 const String token = "";
+
+const List<String> appMusics = [
+  AppMusics.bah,
+  AppMusics.cocaine,
+  AppMusics.fresh,
+  AppMusics.lunnayaSonata,
+  AppMusics.mertvo,
+  AppMusics.nazi,
+  AppMusics.susana,
+  AppMusics.vput,
+  AppMusics.sway,
+  AppMusics.trek,
+  AppMusics.vdx,
+  AppMusics.tchk
+];
+const List<String> appMusicName = [
+  "Бах",
+  "Cocaine",
+  "Фрєш",
+  "Лунная соната ",
+  "Мертвое сердце",
+  "Wir Sind Des Geyers Schwarzer Haufen",
+  "Tom's Diner",
+  "В путь",
+  "Sway",
+  "Трек Салатовый",
+  "Вдохновение",
+  "ТЧК",
+];
+
+const List<String> appNames = [
+  "УННВ",
+  "Lariste",
+  "УННВ",
+  "УННВ",
+  "УННВ",
+  "Botho Lukas Chor",
+  "Suzzane",
+  "УННВ",
+  "Michael Bublé",
+  "УННВ",
+  "УННВ",
+  "УННВ",
+];
+
+const List<String> appImages = [
+  AppImages.bah,
+  AppImages.cocaine,
+  AppImages.vdx,
+  AppImages.lunnayaSonata,
+  AppImages.mertvo,
+  AppImages.nazi,
+  AppImages.suzanne,
+  AppImages.bah,
+  AppImages.sway,
+  AppImages.mertvo,
+  AppImages.vdx,
+  AppImages.tchk,
+];
